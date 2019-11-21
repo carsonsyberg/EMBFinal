2f4f3e6b080101071a2c69148f470f119061e02c
Carson Syberg <44934442+carsonsyberg@users.noreply.github.com>
2019-11-20 19:22:59 -0700

    Add files via upload

--- adc.c
+++ adc.c
@@ -3,41 +3,41 @@
 
 void config_adc(void) {
 
-    // Enable ADC interrupt in NVIC module
-    __enable_interrupt();
-    NVIC_ISER0 = 1 << ((INT_ADC14 - 16) & 31);
+    // Enable ADC interrupt in NVIC module / enable global interrupts
+    __enable_interrupt();//
+    NVIC->ISER[0] = 1 << ((ADC14_IRQn) & 31);//
 
 // Overall - configure ADC core (ADC14CTL0:1)
 
     // when ADC140N = 1 reset removed / core ready
-    ADC14->ADC14CTL0 |= BIT4;
+    ADC14->CTL0 |= ADC14_CTL0_ON;
 
+// HOW TO DECIDE WHAT CLOCK / DIVISORS WE NEED??
     // select clock source with ADC14SSELx bit
-    ADC14->ADC14CTL0 |= BIT3 << 19;
+    ADC14->CTL0 |= BIT3 << 19;
         // divide clock if necessary with ADC14PDIV and ADC14DIV bits
 
-    // disable digital parts of pins P7.6 P7.7 w/ PySELx bits
-
-    // select regular-power mode with ADC14PWRMD
-    // supports sampling rates up to 1 Msps
-    ADC14->ADC14CTL1 &= 0b00;
-
+// HOW TO DECIED WHAT RESOLUTION TO USE??
     // select resolution with ADC14RES bits in ADC14CTL1
-    ADC14->ADC14RES &= 0b01; // 10 bit (11 clock cycle conversion time)
+    //ADC14->RES &= 0b01; // 10 bit (11 clock cycle conversion time)
 
+// HOW TO DECIDE WHAT TO MAKE THESE?
     // select source for SH1 (sample input signal, measurements happen on its rising edge) w/ SHSx bits
-    ADC14CTL0 = ADC14SHT0_2 | ADC14SHP;      // Sampling time, S&H=16
-    // select sample mode or pulse mode w/ ADC14SHP bit
-    // get converted data out from ADC14MEMx register
-    // ADC14IFG.x interrupt is set after measurement
+    ADC14->CTL0 = ADC14_CTL0_SHT0__192 | ADC14_CTL0_SHP;      // Sampling time, S&H=16
 
-    //22.2.6.1 Sampling Stuff
+    ADC14->MCTL[0] = ADC14_MCTLN_INCH_0;    // ref+=AVcc, channel = A0
+    ADC14->MCTL[1] = ADC14_MCTLN_INCH_1 | ADC14_MCTLN_EOS;    // ref+=AVcc, channel = A1 and end of sequence
+
+    ADC14->IER0 = ADC14_IER0_IE1;           // Enable ADC14IFG.1
+    SCB->SCR &= ~SCB_SCR_SLEEPONEXIT_Msk;   // Wake up on exit from ISR
+
+    // Ensures SLEEPONEXIT takes effect immediately
+        __DSB();
+
 
     // 22.2.7 Conversion Memory / Channels Stuff
-
     //select conversion mode repeat-sequence-of-channels
-
     //result stored into ADC14MEMx and ADC14IFG.x set after successful conversion
-
     //use multiple sample and convert function (ADC14MSC) bit 22.2.8.5
 }
+
